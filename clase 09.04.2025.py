4ab6712c0f509db5c332786507c6c2d020266ec6
Genesis Gudiño <gennegudi@gmail.com>
2025-04-09 10:46:31 -0500

    Add files via upload

---
+++ clase 09.04.2025.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+class MiPrimeraClase:
+    pass
+myFirts=MiPrimeraClase()
+class MiPrimeraClase1:
+    pass
+myFirts1=MiPrimeraClase()
+class MiPrimeraClase2:
+    pass
+myFirts2=MiPrimeraClase()
+class MiPrimeraClase3:
+    pass
+myFirts3=MiPrimeraClase()
+class MiPrimeraClase4:
+    pass
+myFirts4=MiPrimeraClase()
+class MiPrimeraClase5:
+    pass
+myFirts5=MiPrimeraClase()
+class MiPrimeraClase6:
+    pass
+myFirts6=MiPrimeraClase()
+
